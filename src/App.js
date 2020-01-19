939aa5f6ce742710bcd445dd9702090d6c637ea3
jlmeister <iamphil4483@yahoo.com>
2020-01-19 17:21:12 -0600

    created HackerNews search engine based on Algolia HN Search API. able to search stories by Date or by Relevance. When the selected search filter is changed, a new fetch request is performed and new results are rendered. final commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,101 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import moment from 'moment';
 
-function App() {
+const Story = (props) => {
+  const { timestamp, title, url, author } = props;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <article>
+      <a href={url} target="_blank"><h1 className='title'>{title}</h1></a>
+      <p className='posted'>{moment(timestamp).fromNow()} | {author}</p>
+    </article>
+  )
+}
+
+const List = ({list}) => {
+  return (
+    list.length > 0 ? (
+      <div>
+        { list.map(result => <Story {...result} />) }
+      </div>
+    ) : (
+      <p>type your query above to search Hacker News...</p>
+    )
+      
+  )
+}
+
+class App extends React.Component {
+  state = {
+    queryText: "",
+    sortBy: "Relevance",
+    results: [],
+    isLoading: false
+  }
+
+  getStories = () => {
+    let url;
+
+    switch (this.state.sortBy) {
+      case 'Date':
+        url = `http://hn.algolia.com/api/v1/search_by_date?query=${this.state.queryText}&tags=story`;
+        break;
+      case 'Relevance':
+        url = `http://hn.algolia.com/api/v1/search?query=${this.state.queryText}&tags=story`;
+        break;
+      default: break;
+    }
+    if (this.state.queryText.length > 0) {
+      this.setState({ isLoading: true });
+      fetch(url)
+        .then(response => response.json())
+        .then(parsedJSON => parsedJSON.hits.map(result => ({
+          timestamp: result.created_at,
+          title: result.title,
+          url: result.url,
+          author: result.author
+        })))
+        .then(posts => this.setState({
+          results: posts,
+          isLoading: false
+        }))
+    }
+  }
+
+  onSubmit = (e) => {
+    e.preventDefault();
+    this.getStories();
+  }
+
+  onQueryTextChange = (e) => {
+    this.setState({
+      queryText: e.target.value
+    })
+  }
+
+  onSelectChange = (e) => {
+    this.setState({
+      sortBy: e.target.value
+    })
+    setTimeout(() => { this.getStories() }, 0);
+  }
+
+  render() {
+    return (
+      <div className="App-header">
+        <h1>Hacker News Search Engine</h1>
+        <form onSubmit={this.onSubmit}>
+          <select className="dropdown" value={this.state.sortBy} onChange={this.onSelectChange}>
+            <option>Date</option>
+            <option>Relevance</option>
+          </select>
+          <input placeholder="type your query here" onChange={this.onQueryTextChange} />
+          <button type="submit">Search</button>
+        </form>
+        <List list={this.state.results} />
+      </div>
+    );
+  }
 }
 
 export default App;
